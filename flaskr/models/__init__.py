7ffea73265a2a942324fff27e679cb2f541c03ef
Jorge Luis Ballesteros Vargas <jorgeballe@gmail.com>
2022-08-23 20:29:01 -0500

    Initial commit

---
+++ flaskr/models/__init__.py
@@ -0,0 +1,8 @@
+from email.mime import image
+from flask_sqlalchemy import SQLAlchemy
+import enum
+
+db = SQLAlchemy()
+
+
+
---
+++ flaskr/models/categorias.py
@@ -0,0 +1,4 @@
+from flaskr.models import db
+
+class Categorias(db.make_connector):
+    
